91428efad773929a9cedc181bdf3448dde547271
bdutta123 <91731885+bdutta123@users.noreply.github.com>
2024-06-17 12:18:38 +0530

    Create 50000 branches and tags

--- gittest.sh
+++ gittest.sh
@@ -6,17 +6,17 @@ REPO_NAME="Mainrepo1"
 
 # Create 50,000 branches
 for i in {1..10}; do
-git branch "feature-branch-$i"
+git branch "feature-dev-branch-$i"
 done
 
 # Create 50,000 tags
 for i in {1..10}; do
-git tag "release-tag-$i"
+git tag "release1.1-tag-$i"
 done
 
 # Add and commit the changes
 git add .
-git commit -m "Create 10 branches and tags"
+git commit -m "Create 50000 branches and tags"
 
 # Push the changes to GitHub
 git push origin --all
